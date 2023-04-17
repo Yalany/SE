6599558586f697d544cf8946c9e3390bd74dcfdd
Nikita Dobrinskiy <Nikita_Dobrinskiy@epam.com>
2023-04-17 08:54:09 +0300

    Reindent

--- src/main/java/game/EventDeck.java
+++ src/main/java/game/EventDeck.java
@@ -4,78 +4,78 @@ import java.util.stream.IntStream;
 
 public class EventDeck {
 
-    private final int size; // size of the deck
-    private final boolean isEndless; // if true deck will loop indefinitely and popped events will be reset to 0
-    private final int[] events; // 0 is no event present (aka random event), number is event id in db
+  private final int size; // size of the deck
+  private final boolean isEndless; // if true deck will loop indefinitely and popped events will be reset to 0
+  private final int[] events; // 0 is no event present (aka random event), number is event id in db
 
-    private int pointer; // points at next player event
+  private int pointer; // points at next player event
 
-    public EventDeck(int size, boolean isEndless) {
-        this.size = size;
-        this.isEndless = isEndless;
-        this.events = new int[size];
-        IntStream.range(0, size - 1).forEach(i -> this.events[i] = 0);
-        this.pointer = 0;
-    }
+  public EventDeck(int size, boolean isEndless) {
+    this.size = size;
+    this.isEndless = isEndless;
+    this.events = new int[size];
+    IntStream.range(0, size - 1).forEach(i -> this.events[i] = 0);
+    this.pointer = 0;
+  }
 
-    // puts event in place of first random event starting from specified offset
-    void shuffleIn(int eventId, int offset) {
-        if (offset < 0)
-            throw new IllegalArgumentException("offset can't be lower than 0");
-        if (offset >= size)
-            throw new IllegalArgumentException("offset can't be greater or equal than deck size");
+  // puts event in place of first random event starting from specified offset
+  void shuffleIn(int eventId, int offset) {
+    if (offset < 0)
+      throw new IllegalArgumentException("offset can't be lower than 0");
+    if (offset >= size)
+      throw new IllegalArgumentException("offset can't be greater or equal than deck size");
 
-        var currentPointer = pointer;
+    var currentPointer = pointer;
 
-        // resolving case when event wanted to be shuffled in from very top of deck
-        if (offset == 0) {
-            if (events[pointer] == 0) {
-                events[pointer] = eventId;
-                return;
-            }
-            movePointerBy(1);
-        }
-
-        movePointerBy(offset);
-
-        // traversing deck in search of room for event
-        while (events[pointer] != 0 && pointer != currentPointer)
-            movePointerBy(1);
-
-        // if pointers match then there is no room in deck for event
-        if (pointer == currentPointer)
-            return;
-
+    // resolving case when event wanted to be shuffled in from very top of deck
+    if (offset == 0) {
+      if (events[pointer] == 0) {
         events[pointer] = eventId;
-        pointer = currentPointer;
+        return;
+      }
+      movePointerBy(1);
     }
 
-    void putOnTop(int eventId) {
-        movePointerBy(-1);
-        events[pointer] = eventId;
-    }
+    movePointerBy(offset);
 
-    int pop() {
-        var event = events[pointer];
-        if (isEndless) events[pointer] = 0;
-        else events[pointer] = -1;
-        movePointerBy(1);
-        return event;
-    }
+    // traversing deck in search of room for event
+    while (events[pointer] != 0 && pointer != currentPointer)
+      movePointerBy(1);
 
-    private void movePointerBy(int steps) {
-        if (steps < -1)
-            throw new IllegalArgumentException("pointer steps can't be lower than -1");
+    // if pointers match then there is no room in deck for event
+    if (pointer == currentPointer)
+      return;
 
-        if (steps == -1) {
-            if (pointer + steps < 0)
-                pointer += size;
-            pointer += steps;
-        }
-        if (steps > 0) {
-            if (pointer + steps >= size)
-                pointer -= size;
-            pointer += steps;
-        }
+    events[pointer] = eventId;
+    pointer = currentPointer;
+  }
+
+  void putOnTop(int eventId) {
+    movePointerBy(-1);
+    events[pointer] = eventId;
+  }
+
+  int pop() {
+    var event = events[pointer];
+    if (isEndless) events[pointer] = 0;
+    else events[pointer] = -1;
+    movePointerBy(1);
+    return event;
+  }
+
+  private void movePointerBy(int steps) {
+    if (steps < -1)
+      throw new IllegalArgumentException("pointer steps can't be lower than -1");
+
+    if (steps == -1) {
+      if (pointer + steps < 0)
+        pointer += size;
+      pointer += steps;
     }
+    if (steps > 0) {
+      if (pointer + steps >= size)
+        pointer -= size;
+      pointer += steps;
+    }
+  }
 }
--- src/main/java/game/GameResources.java
+++ src/main/java/game/GameResources.java
@@ -5,65 +5,65 @@ import java.util.Map;
 
 public final class GameResources {
 
-    private final Map<String, GameResource> resources;
+  private final Map<String, GameResource> resources;
 
-    private GameResources(Map<String, GameResource> resources) {
-        this.resources = resources;
+  private GameResources(Map<String, GameResource> resources) {
+    this.resources = resources;
+  }
+
+  String getDisplayName(String key) {
+    return resources.get(key).getDisplayName();
+  }
+
+  public int getAmount(String key) {
+    return resources.get(key).getAmount();
+  }
+
+  void modifyAmount(String key, int modification) {
+    resources.get(key).modifyAmount(modification);
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  private static final class GameResource {
+    private final String displayName;
+    private int amount;
+
+    GameResource(String displayName, int amount) {
+      this.displayName = displayName;
+      this.amount = amount;
     }
 
-    String getDisplayName(String key) {
-        return resources.get(key).getDisplayName();
+    String getDisplayName() {
+      return displayName;
     }
 
-    public int getAmount(String key) {
-        return resources.get(key).getAmount();
+    int getAmount() {
+      return amount;
     }
 
-    void modifyAmount(String key, int modification) {
-        resources.get(key).modifyAmount(modification);
+    void modifyAmount(int modification) {
+      this.amount += modification;
+    }
+  }
+
+  public static final class Builder {
+
+    private final HashMap<String, GameResource> resources;
+
+    private Builder() {
+      resources = new HashMap<>();
     }
 
-    public static Builder builder() {
-        return new Builder();
+    public Builder addResource(String key, String displayName, int startingValue) {
+      resources.put(key, new GameResource(displayName, startingValue));
+      return this;
     }
 
-    private static final class GameResource {
-        private final String displayName;
-        private int amount;
-
-        GameResource(String displayName, int amount) {
-            this.displayName = displayName;
-            this.amount = amount;
-        }
-
-        String getDisplayName() {
-            return displayName;
-        }
-
-        int getAmount() {
-            return amount;
-        }
-
-        void modifyAmount(int modification) {
-            this.amount += modification;
-        }
+    public GameResources build() {
+      return new GameResources(resources);
     }
-
-    public static final class Builder {
-
-        private final HashMap<String, GameResource> resources;
-
-        private Builder() {
-            resources = new HashMap<>();
-        }
-
-        public Builder addResource(String key, String displayName, int startingValue) {
-            resources.put(key, new GameResource(displayName, startingValue));
-            return this;
-        }
-
-        public GameResources build() {
-            return new GameResources(resources);
-        }
-    }
+  }
 }
