7cf59622310bfa8432922f964953a1eb6106d105
Nikita Dobrinskiy <Nikita_Dobrinskiy@epam.com>
2023-04-19 06:17:25 +0300

    GameState serialization behavior

---
+++ src/main/java/game/Config.java
@@ -0,0 +1,9 @@
+package game;
+
+import com.google.gson.Gson;
+
+class Config {
+  public final static String GAME_DATA_DIRECTORY = "../user_data/";
+  public final static String JSON_POSTFIX = ".json";
+  public final static Gson GSON = new Gson();
+}
--- src/main/java/game/GameState.java
+++ src/main/java/game/GameState.java
@@ -1,8 +1,32 @@
 package game;
 
+import com.google.gson.annotations.SerializedName;
+import util.FileUtils;
+
 public class GameState {
-  private String gameId;
-  private PlayerResources resources;
+  @SerializedName("user_id")
+  private String userId;
+
+  @SerializedName("player_resources")
+  private PlayerResources playerResources;
+
+  @SerializedName("event_deck")
   private EventDeck eventDeck;
+
+  @SerializedName("deck_pointer")
   private int deckPointer;
+
+  GameState(final String userId) {
+    this.userId = userId;
+    if (FileUtils.fileExists(path(userId)))
+      Config.GSON.fromJson(FileUtils.readFile(path(userId)), GameState.class);
+  }
+
+  void save() {
+    FileUtils.writeFile(path(userId), Config.GSON.toJson(this));
+  }
+
+  private static String path(final String userId) {
+    return Config.GAME_DATA_DIRECTORY + userId + Config.JSON_POSTFIX;
+  }
 }
